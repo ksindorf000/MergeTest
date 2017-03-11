0d19fcad856d5f11ab1d3e0b16e7af8900a89927
ksindorf000 <ksindorf000@gmail.com>
2017-03-10 21:55:21 -0500

    Add project files.

---
+++ MergeTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MergeTest.MvcApplication" Language="C#" %>
---
+++ MergeTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MergeTest.Startup))]
+namespace MergeTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
