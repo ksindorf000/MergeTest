6d9725c762da807fddb0d936b84ba6fc5bc1b4cd
ksindorf000 <ksindorf000@gmail.com>
2017-03-12 22:18:27 -0400

    Added custom route to user profiles

--- MergeTest/Controllers/UserController.cs
+++ MergeTest/Controllers/UserController.cs
@@ -1,15 +1,43 @@
-﻿using System;
+﻿using MergeTest.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
 using System.Web.Mvc;
 
 namespace MergeTest.Controllers
 {
     public class UserController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+        ApplicationUser user = new ApplicationUser();
 
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string userId = User.Identity.GetUserId();
+                user = db.Users
+                    .Where(u => u.Id == userId)
+                    .FirstOrDefault();
+
+                return View(user);
+            }
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        [Route("send/{username}")]
+        public ActionResult Index(string username)
+        {
+            user = db.Users
+                .Where(u => u.UserName == username)
+                .FirstOrDefault();
+
+            return View(user);
+        }
     }
 }
